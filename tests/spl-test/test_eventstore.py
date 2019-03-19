250287d62f2490a037374c625c801548c1a5d3ec
markheger <mark-oliver.heger@de.ibm.com>
2019-03-19 14:18:31 +0100

    resolves #38, resolves #39, resolves #40

--- tests/spl-test/test_eventstore.py
+++ tests/spl-test/test_eventstore.py
@@ -2,15 +2,28 @@ import unittest
 
 from streamsx.topology.topology import *
 from streamsx.topology.tester import Tester
+from streamsx.topology.state import ConsistentRegionConfig
 import streamsx.spl.op as op
 import streamsx.spl.toolkit as tk
-import os
 import streamsx.rest as sr
+import streamsx.spl.types as spltypes
+import streamsx.eventstore as es
+
+import os
+import subprocess
 
 import streamsx.topology.context
 import requests
 from urllib.parse import urlparse
 
+
+def streams_install_env_var():
+    result = True
+    try:
+        os.environ['STREAMS_INSTALL']
+    except KeyError: 
+        result = False
+    return result
 
 class TestDistributed(unittest.TestCase):
     """ Test invocations of composite operators in local IBM Streams instance """
@@ -30,7 +43,8 @@ class TestDistributed(unittest.TestCase):
         self._use_local_toolkit()
 
     def _add_toolkits(self, topo, toolkit_name):
-        tk.add_toolkit(topo, toolkit_name)
+        if toolkit_name is not None:
+            tk.add_toolkit(topo, toolkit_name)
         if self.eventstore_toolkit_location is not None:
             tk.add_toolkit(topo, self.eventstore_toolkit_location)
 
@@ -59,20 +73,114 @@ class TestDistributed(unittest.TestCase):
         tester.test(self.test_ctxtype, cfg, always_collect_logs=True)
         print (str(tester.result))
 
+    def _build_only(self, name, topo):
+        result = streamsx.topology.context.submit("TOOLKIT", topo.graph) # creates tk* directory
+        print(name + ' (TOOLKIT):' + str(result))
+        assert(result.return_code == 0)
+        result = streamsx.topology.context.submit("BUNDLE", topo.graph)  # creates sab file
+        print(name + ' (BUNDLE):' + str(result))
+        assert(result.return_code == 0)
+
+    def _index_tk(self, tkdir):
+        si = os.environ['STREAMS_INSTALL']
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+        tkl = this_dir+'/'+tkdir
+        ri = subprocess.call([os.path.join(si, 'bin', 'spl-make-toolkit'), '-i', tkl])
 
     def test_insert_sample_flush_remaining_tuples(self):
+        print ('\n---------'+str(self))
+        name = 'test_insert_sample_flush_remaining_tuples'
         # test the sample application
         # final marker should flush the remaining tuples
         num_expected = 305
         batch_size = 50
-        self._build_launch_validate("test_insert_sample_flush_remaining_tuples", "com.ibm.streamsx.eventstore.sample::InsertSampleComp", {'connectionString': self.connection, 'batchSize':batch_size, 'iterations': num_expected}, '../../samples/EventStoreInsertSample', num_expected, True)
+        self._build_launch_validate(name, "com.ibm.streamsx.eventstore.sample::InsertSampleComp", {'connectionString': self.connection, 'batchSize':batch_size, 'iterations': num_expected}, '../../samples/EventStoreInsertSample', num_expected, True)
 
     def test_insert_sample_batch_complete(self):
+        print ('\n---------'+str(self))
+        name = 'test_insert_sample_batch_complete'
         # test the sample application
         # final marker received after last async batch is triggered
         num_expected = 300
         batch_size = 50
-        self._build_launch_validate("test_insert_sample_batch_complete", "com.ibm.streamsx.eventstore.sample::InsertSampleComp", {'connectionString': self.connection, 'batchSize':batch_size, 'iterations': num_expected}, '../../samples/EventStoreInsertSample', num_expected, True)
+        self._build_launch_validate(name, "com.ibm.streamsx.eventstore.sample::InsertSampleComp", {'connectionString': self.connection, 'batchSize':batch_size, 'iterations': num_expected}, '../../samples/EventStoreInsertSample', num_expected, True)
+
+
+    def test_insert_consistent_region(self):
+        print ('\n---------'+str(self))
+        name = 'test_insert_consistent_region'
+        topo = Topology(name)
+        self._add_toolkits(topo, None)
+        # configuration of consistent region trigger period
+        trigger_period = 10
+        num_expected_tuples = 8000
+        num_resets = 2
+        run_for = 120 # in seconds
+
+        beacon = op.Source(topo, "spl.utility::Beacon",
+            'tuple<int64 id, rstring val>',
+            params = {'period': 0.01, 'iterations': num_expected_tuples})
+        beacon.id = beacon.output('(int64)IterationCount()')
+        beacon.val = beacon.output(spltypes.rstring('CR_TEST'))
+        beacon.stream.set_consistent(ConsistentRegionConfig.periodic(trigger_period))
+        
+        es.insert(beacon.stream, self.connection, 'TESTDB', 'CRTable', primary_key='id')
+        
+        #self._build_only(name, topo)
+
+        tester = Tester(topo)
+        tester.run_for(run_for)
+        tester.resets(num_resets) # minimum number of resets for each region
+
+        cfg = {}
+        if ("TestICP" in str(self)):
+            cfg = self._service()
+
+        # change trace level
+        job_config = streamsx.topology.context.JobConfig(tracing='warn')
+        job_config.add(cfg)
+
+        cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False
+
+        tester.test(self.test_ctxtype, cfg, always_collect_logs=True)
+        print (str(tester.result))
+
+    @unittest.skipIf(streams_install_env_var() == False, "Missing STREAMS_INSTALL environment variable.")
+    def test_compile_time_error_checkpoint_periodic(self):
+        print ('\n---------'+str(self))
+        if self.eventstore_toolkit_location is not None:
+            test_toolkit = 'compile.test'
+            self._index_tk(test_toolkit)
+            # compile test only
+            r = op.main_composite(kind='com.ibm.streamsx.eventstore.test::Test_checkpoint_periodic', toolkits=[self.eventstore_toolkit_location, test_toolkit])
+            rc = streamsx.topology.context.submit('BUNDLE', r[0])
+            #expect compile error
+            self.assertEqual(1, rc['return_code'])
+
+    @unittest.skipIf(streams_install_env_var() == False, "Missing STREAMS_INSTALL environment variable.")
+    def test_compile_time_error_checkpoint_operator_driven(self):
+        print ('\n---------'+str(self))
+        if self.eventstore_toolkit_location is not None:
+            test_toolkit = 'compile.test'
+            self._index_tk(test_toolkit)
+            # compile test only
+            r = op.main_composite(kind='com.ibm.streamsx.eventstore.test::Test_checkpoint_operatorDriven', toolkits=[self.eventstore_toolkit_location, test_toolkit])
+            rc = streamsx.topology.context.submit('BUNDLE', r[0])
+            #expect compile error
+            self.assertEqual(1, rc['return_code'])
+
+    @unittest.skipIf(streams_install_env_var() == False, "Missing STREAMS_INSTALL environment variable.")
+    def test_compile_time_error_consistent_region_unsupported_configuration(self):
+        print ('\n---------'+str(self))
+        if self.eventstore_toolkit_location is not None:
+            test_toolkit = 'compile.test'
+            self._index_tk(test_toolkit)
+            # compile test only
+            r = op.main_composite(kind='com.ibm.streamsx.eventstore.test::Test_consistent_region_unsupported_configuration', toolkits=[self.eventstore_toolkit_location, test_toolkit])
+            rc = streamsx.topology.context.submit('BUNDLE', r[0])
+            #expect compile error
+            self.assertEqual(1, rc['return_code'])
+
 
 
 class TestICP(TestDistributed):
